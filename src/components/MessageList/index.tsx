26984743dc12fb949e92428c04fe4ea5ab10e919
Mateus Santos <m.santos.azeve@gmail.com>
2021-10-25 21:40:57 -0300

    layout esquerdo da pagina lista de mensagens

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,61 @@
 import styles from "./styles.module.scss";
 
+import logoImg from "../../assets/logo.svg";
+
 export function MessageList() {
-  return <h1>MessageList</h1>;
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/mateus-azevedo.png"
+                alt="Mateus Santos"
+              />
+            </div>
+            <span>Mateus Santos</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/mateus-azevedo.png"
+                alt="Mateus Santos"
+              />
+            </div>
+            <span>Mateus Santos</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/mateus-azevedo.png"
+                alt="Mateus Santos"
+              />
+            </div>
+            <span>Mateus Santos</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
