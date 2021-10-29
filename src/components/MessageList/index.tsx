c577904deeaa099dba1f1058569d4356fdc7a66e
Mateus Santos <m.santos.azeve@gmail.com>
2021-10-28 23:45:02 -0300

    atualizacao mensagens - socket recupera novas mensagens e empurra na fila

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import io from "socket.io-client";
 import { api } from "../../services";
 
 import styles from "./styles.module.scss";
@@ -14,8 +15,28 @@ type Message = {
   };
 };
 
+const messagesQueue: Message[] = [];
+
+const socket = io("http://localhost:4000");
+
+socket.on("new_message", (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+});
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages((prevState) =>
+          [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean)
+        );
+
+        messagesQueue.shift();
+      }
+    }, 3000);
+  }, []);
 
   useEffect(() => {
     // CHAMADA PARA API
